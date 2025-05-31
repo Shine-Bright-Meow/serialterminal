3c91be1091013fcb300b30eb7e1cf7e569769cf8
ShineBrightMeow <142170650+ShineBrightMeow@users.noreply.github.com>
2025-05-30 20:12:15 -0700

    Update Webpage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,29 +6,29 @@ import Typography from '@mui/material/Typography'
 
 import version from '../version.js'
 
-// const Footer = () => {
-//     return (
-//         <Box sx={{ marginTop: 'auto' }}>
-//             { /* Made in Germany :D */}
-//             <Box sx={{ mx: 'auto', mt: 0 }}>
-//                 <Typography
-//                 align='center'
-//                 display='block'>
-//                 Made with ❤️ by <Link href='https://spacehuhn.com' target='_blank' underline='hover' color='inherit'>Spacehuhn</Link>
-//                 </Typography>
-//             </Box>
+const Footer = () => {
+    return (
+        <Box sx={{ marginTop: 'auto' }}>
+            { /* Made in Germany :D */}
+            <Box sx={{ mx: 'auto', mt: 0 }}>
+                <Typography
+                align='center'
+                display='block'>
+                <Link href='#' target='_blank' underline='hover' color='inherit'>Meow~!</Link>
+                </Typography>
+            </Box>
 
-//             { /* Version */}
-//             <Typography
-//                 variant='caption'
-//                 align='center'
-//                 display='block'
-//                 sx={{ color: '#ddd' }}>
-//                 <Link href='https://github.com/spacehuhntech/serial' target='_blank' underline='hover' color='inherit'>{version.name}</Link>
-//             </Typography>
-//         </Box>
-//     )
-// }
+            { /* Version */}
+            <Typography
+                variant='caption'
+                align='center'
+                display='block'
+                sx={{ color: '#ddd' }}>
+                <Link href='#' target='_blank' underline='hover' color='inherit'>{version.name}</Link>
+            </Typography>
+        </Box>
+    )
+}
 
 export default Footer
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,53 +7,52 @@ import Button from '@mui/material/Button'
 import AddBoxIcon from '@mui/icons-material/AddBox'
 import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 
-// const Header = () => {
-//     return (
-//         <AppBar
-//             position='static'
-//             sx={{
-//                 background: '#2c387e',
-//             }}
-//         >
-//             <Toolbar>
-//                 <img
-//                     src='logo768.png'
-//                     alt='Logo'
-//                     height='30px'
-//                 />
+const Header = () => {
+    return (
+        <AppBar
+            position='static'
+            sx={{
+                background: '#2c387e',
+            }}
+        >
+            <Toolbar>
+                <img
+                    src='logo768.png'
+                    alt='Logo'
+                    height='30px'
+                />
 
-//                 <Typography
-//                     variant='h6'
-//                     component='h1'
-//                     noWrap
-//                     sx={{ 
-//                         flexGrow: 1,
-//                         fontFamily: 'Bungee',
-//                     }}
-//                 >
-//                     &nbsp;&nbsp;Serial Terminal
-//                 </Typography>
+                <Typography
+                    variant='h6'
+                    component='h1'
+                    noWrap
+                    sx={{ 
+                        flexGrow: 1,
+                        fontFamily: 'Bungee',
+                    }}
+                >
+                    &nbsp;&nbsp;Serial Terminal
+                </Typography>
 
-//                 <Button
-//                     //variant='contained'
-//                     //color='success'
-//                     sx={{ color: '#fff' }}
-//                     href='https://huhn.me/'
-//                     target='_blank'
-//                     endIcon={<OpenInNewIcon />}>
-//                     More Tools
-//                 </Button>
+                <Button
+                    //variant='contained'
+                    //color='success'
+                    sx={{ color: '#fff' }}
+                    href='#'
+                    target='_blank'
+                    endIcon={<OpenInNewIcon />}>
+                </Button>
 
-//                 <Button
-//                     sx={{ color: '#fff' }}
-//                     target='_blank'
-//                     href='#'
-//                 >
-//                     <AddBoxIcon/>
-//                 </Button>
-//             </Toolbar>
-//         </AppBar>
-//     )
-// }
+                <Button
+                    sx={{ color: '#fff' }}
+                    target='_blank'
+                    href='#'
+                >
+                    <AddBoxIcon/>
+                </Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
 
 export default Header
